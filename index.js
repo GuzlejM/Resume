1e8560d255d455a96b6816a2a67a0bbe92a20dae
Martin <martin.guzlej@gmail.com>
2020-09-10 08:44:28 +0100

    Finished Ready to push to Heroku

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+app = express();
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.render(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server on!")
+}); 
